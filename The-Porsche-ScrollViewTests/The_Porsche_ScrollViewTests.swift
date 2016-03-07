805783ded4d4def963962c8ccadab2d10f402b2e
Admin <admin@mac-admin.local>
2016-03-07 15:53:59 +0300

    Initial Commit

---
+++ The-Porsche-ScrollViewTests/The_Porsche_ScrollViewTests.swift
@@ -0,0 +1,36 @@
+//
+//  The_Porsche_ScrollViewTests.swift
+//  The-Porsche-ScrollViewTests
+//
+//  Created by Admin on 07/03/16.
+//  Copyright © 2016 Admin. All rights reserved.
+//
+
+import XCTest
+@testable import The_Porsche_ScrollView
+
+class The_Porsche_ScrollViewTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
